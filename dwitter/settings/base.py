dceee0819a85f7da6148ee25da23fba770b656d5
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2016-01-07 01:23:59 +0100

    fix prod 404 on login redirects

--- dwitter/settings/base.py
+++ dwitter/settings/base.py
@@ -16,12 +16,11 @@ import os
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 BASE_URL = 'http://www.lionleaf.org/dwitter'
 
-
 REGISTRATION_OPEN = True        # If True, users can register
 ACCOUNT_ACTIVATION_DAYS = 7     # One-week activation window; you may, of course, use a different value.
 REGISTRATION_AUTO_LOGIN = True  # If True, the user will be automatically logged in.
-LOGIN_REDIRECT_URL = '/'        # The page you want users to arrive at after they successful log in
-LOGIN_URL = '/accounts/login/'  # The page users are directed to if they are not logged in,
+LOGIN_REDIRECT_URL = BASE_URL        # The page you want users to arrive at after they successful log in
+LOGIN_URL = BASE_URL + '/accounts/login/'  # The page users are directed to if they are not logged in,
                                 # and are trying to access pages requiring authentication
 
 # Quick-start development settings - unsuitable for production
