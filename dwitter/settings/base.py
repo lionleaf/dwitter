76a4708c0d22a8589e8471cbf4e55097ba3ebe26
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2016-01-17 00:59:57 +0100

    Move css to external file

--- dwitter/settings/base.py
+++ dwitter/settings/base.py
@@ -141,4 +141,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, "static/")
+STATIC_ROOT = os.path.join(BASE_DIR, "dwitter/static/")
