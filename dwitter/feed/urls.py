ac8d93ee607adcdb7e6438b6c3d622c20a5c3b95
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2016-01-25 02:50:58 +0100

    play/pause on scrolling

--- dwitter/feed/urls.py
+++ dwitter/feed/urls.py
@@ -3,7 +3,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-        url(r'^$', views.feed,{'page_nr' : 1}, name='feed'),
+        url(r'^$', views.feed,{'page_nr' : 1}, name='root'),
         url(r'^page/(?P<page_nr>\d+)$', views.feed, name='feed'),
         url(r'^dweet$', views.dweet, name='dweet'),
         url(r'^like/(?P<post_id>\d+)$', views.like, name='like'),
