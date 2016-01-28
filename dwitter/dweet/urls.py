28d8df46e8c9e17743599f9314dbbdc0b0c17fe8
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2016-01-28 03:09:30 +0100

    Preview now uses dweet.lionleaf too!

--- dwitter/dweet/urls.py
+++ dwitter/dweet/urls.py
@@ -4,4 +4,5 @@ from . import views
 
 urlpatterns = [
         url(r'^id/(?P<dweet_id>[0-9]*)$', views.fullscreen_dweet, name='fullscreen_dweet'),
+        url(r'^blank$', views.blank_dweet, name='blank_dweet'),
 ]
--- dwitter/dweet/views.py
+++ dwitter/dweet/views.py
@@ -11,6 +11,13 @@ from django.views.decorators.clickjacking import xframe_options_exempt
 def fullscreen_dweet(request, dweet_id):
   dweet = get_object_or_404(Dweet, id=dweet_id) 
 
-  context = {'dweet': dweet
+  context = {'code': dweet.code
             }
-  return render(request, 'dweet/dweet-id.html', context );
+  return render(request, 'dweet/dweet.html', context );
+
+
+@xframe_options_exempt
+def blank_dweet(request):
+  context = {'code': "c.width=1920;x.fillRect(800+S(t)*300,400,200,200)"
+            }
+  return render(request, 'dweet/dweet.html', context );
