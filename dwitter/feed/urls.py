1f1786f7ed438e6f386eac5b16774eb3f3199ea9
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2016-03-03 23:01:10 +0100

    Change urls away from /dweet/ as they are special-cased

--- dwitter/feed/urls.py
+++ dwitter/feed/urls.py
@@ -5,8 +5,8 @@ from . import views
 urlpatterns = [
         url(r'^$', views.feed,{'page_nr' : 1}, name='root'),
         url(r'^page/(?P<page_nr>\d+)$', views.feed, name='feed'),
-        url(r'^dweet/reply/(?P<dweet_id>\d+)$', views.dweet_reply, name='dweet_reply'),
-        url(r'^dweet/delete/(?P<dweet_id>\d+)$', views.dweet_delete, name='dweet_delete'),
+        url(r'^dweetreply/(?P<dweet_id>\d+)$', views.dweet_reply, name='dweet_reply'),
+        url(r'^dweetdelete/(?P<dweet_id>\d+)$', views.dweet_delete, name='dweet_delete'),
         url(r'^dweet$', views.dweet, name='dweet'),
         url(r'^like/(?P<post_id>\d+)$', views.like, name='like'),
 ]
