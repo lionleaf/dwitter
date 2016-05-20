2af98bf0e9f6eb35196cc70f6eb7fa09694471c6
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2016-05-20 07:29:43 +0200

    Make post comment happen through AJAX

--- dwitter/static/js/ajax-handling.js
+++ dwitter/static/js/ajax-handling.js
@@ -1,9 +1,8 @@
-var processLike = function()  {
- 
-   var $like_button = $(this);
- 
-   var dweet_id = $like_button.data('dweet_id');
 
+
+var processLike = function()  {
+   var $like_button = $(this);
+   var dweet_id = $like_button.data('dweet_id');
    var processServerResponse = function(serverResponse_json, textStatus_ignored,
        jqXHR_ignored)  {
      if(serverResponse_json.not_authenticated){
@@ -26,7 +25,9 @@ var processLike = function()  {
    $.ajax(config);
 };
 
-
+var getCommentHTML = function(comment) {
+       return '<div class=comment><p><span class=comment-name>'+comment.author+':</span>'+comment.text+'</p></div>';
+}
 var loadComments = function() {
   var step = 1000;
   var current_offset = $(this).data('offset') ;
@@ -51,7 +52,7 @@ var loadComments = function() {
        var comment = serverResponse_json.results[i];
        console.log(comment);
        console.log($comment_section);
-       new_comment_list  += '<div class=comment><p><span class=comment-name>'+comment.author+':</span>'+comment.text+'</p></div>';
+       new_comment_list  += getCommentHTML(comment);
      }
      $comment_section[0].innerHTML = new_comment_list + $comment_section[0].innerHTML;
      
@@ -65,8 +66,41 @@ var loadComments = function() {
    $.ajax(config);
 }
 
+var postComment = function() {
+  var $post_comment_button = $(this);
+  var dweet_id = $post_comment_button.data('dweet_id');
+  var csrf = $post_comment_button.data('csrf');
+  console.log("csrf: " + csrf);
+  var $comment_text = $post_comment_button.siblings('.comment-input');
+  var $comment_section = $post_comment_button.closest('.comment-section').children('.comments');
+
+  var postCommentResponse = function(serverResponse_json, textStatus_ignored,
+      jqXHR_ignored)  {
+
+    $comment_text[0].value = '';
+    $comment_section[0].innerHTML =  $comment_section[0].innerHTML + getCommentHTML(serverResponse_json);
+  }
+
+  var comment = {
+    reply_to: dweet_id,
+    text: $comment_text[0].value,
+    csrfmiddlewaretoken: csrf,
+  }
+
+  var config = {
+    url: 'api/comments/',
+    method: 'POST',
+    success: postCommentResponse,
+    error: postCommentResponse,
+    data: comment,
+  };
+  console.log(config);
+  $.ajax(config);
+}
+
 
 $(document).ready(function()  {
   $('body').on('click','.like-button', processLike);
   $('body').on('click','.load-comments-link', loadComments);
+  $('body').on('click','.comment-submit', postComment);
 });
