64aee942bb6fa77c66a19e587b40c0ba8b6b858e
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2016-05-20 20:07:41 +0200

    Restrict modification of comments to author using permissions

--- dwitter/views.py
+++ dwitter/views.py
@@ -3,11 +3,12 @@ from django.http import HttpResponseRedirect, HttpResponse, Http404
 from django.core.urlresolvers import reverse
 from dwitter.models import Dweet
 from dwitter.models import Comment
+from dwitter.permissions import IsAuthorOrReadOnly
 from dwitter.serializers import CommentSerializer, DweetSerializer, UserSerializer
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 from django.utils import timezone
-from rest_framework import status, viewsets
+from rest_framework import status, viewsets, permissions
 from rest_framework.decorators import detail_route, list_route
 from rest_framework.response import Response
 
@@ -15,6 +16,8 @@ class CommentViewSet(viewsets.ModelViewSet):
   queryset = Comment.objects.all()
   serializer_class = CommentSerializer
   filter_fields = ('reply_to', 'author')
+  permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+                        IsAuthorOrReadOnly, )
 
   def perform_create(self, serializer):
     serializer.save(author=self.request.user, posted=timezone.now())
