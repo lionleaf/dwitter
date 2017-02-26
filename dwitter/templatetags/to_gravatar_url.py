9146d7eb65503227a1974d41bfa61d1ef892e8eb
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2017-02-26 13:09:04 +0100

    Add user avatars from gravatar

---
+++ dwitter/templatetags/to_gravatar_url.py
@@ -0,0 +1,10 @@
+import hashlib
+from django import template
+
+register = template.Library()
+
+
+@register.filter
+def to_gravatar_url(email):
+    return ('https://gravatar.com/avatar/%s?d=retro' %
+            hashlib.md5((email or '').strip().lower()).hexdigest())
