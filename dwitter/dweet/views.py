fc3cfa10d88a9cd5bd40e3ca3231b6bf2656c5ad
Thomas Broadley <buriedunderbooks@hotmail.com>
2017-03-05 22:13:18 -0500

    different 404 page for dweet subdomain

--- dwitter/dweet/views.py
+++ dwitter/dweet/views.py
@@ -1,6 +1,16 @@
 from django.shortcuts import get_object_or_404, render
 from dwitter.models import Dweet
 from django.views.decorators.clickjacking import xframe_options_exempt
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+
+def handler404(request):
+    template = '404_dweet.html' if request.subdomain == 'dweet' else '404.html'
+    response = render_to_response(template, {},
+                                  context_instance=RequestContext(request))
+    response.status_code = 404
+    return response
 
 
 @xframe_options_exempt
