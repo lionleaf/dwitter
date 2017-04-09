752d9634ea5ce6e0245edbf171693792b0fd76c6
Paul Joey Clark <joey@neuralyte.org>
2017-04-09 17:48:45 +0800

    Add a feed page which shows old dweets randomly

--- dwitter/feed/urls.py
+++ dwitter/feed/urls.py
@@ -23,6 +23,11 @@ urlpatterns = [
     url(r'^new/page/(?P<page_nr>\d+)$',
         views.feed, {'sort': 'new'}, name='new_feed_page'),
 
+    url(r'^old$',
+        views.feed, {'page_nr': 1, 'sort': 'old'}, name='old_feed'),
+    url(r'^old/page/(?P<page_nr>\d+)$',
+        views.feed, {'sort': 'old'}, name='old_feed_page'),
+
     url(r'^d/(?P<dweet_id>\d+)$',
         views.dweet_show, name='dweet_show'),
     url(r'^d/(?P<dweet_id>\d+)/reply$',
