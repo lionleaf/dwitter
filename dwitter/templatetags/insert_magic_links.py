624d7367a6fcd220298ab92705094747a3e26e4a
Ates Goral <ates.goral@shopify.com>
2018-01-02 18:53:18 -0500

    Be picky about what comes before and after

--- dwitter/templatetags/insert_magic_links.py
+++ dwitter/templatetags/insert_magic_links.py
@@ -5,17 +5,18 @@ from django.core.urlresolvers import reverse
 register = template.Library()
 
 
-def to_link(match):
-    dweet_id = match.group(1)
-    username = match.group(2)
+def to_link(m):
+    text = m.group('text')
+    dweet_id = m.group('dweet_id')
+    username = m.group('username')
 
     if username is None:
         path = reverse('dweet_show', kwargs={'dweet_id': dweet_id})
     else:
         path = reverse('user_feed', kwargs={'url_username': username})
 
-    return '<a href="%s">%s</a>' % (path, match.group(0))
+    return '<a href="%s">%s</a>' % (path, text)
 
 @register.filter(is_safe=True)
 def insert_magic_links(text):
-    return re.sub(r'/d/(\d+)|/u/([\w.@+-]+)', to_link, text)
+    return re.sub(r'(?:^|(?<=\s))(?P<text>/d/(?P<dweet_id>\d+)|/u/(?P<username>[\w.@+-]+))(?=$|\s)', to_link, text)
