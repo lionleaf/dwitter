ed4a373605ed39a7bce7cecf435463b62250981b
Ates Goral <ates.goral@shopify.com>
2018-01-03 02:56:42 -0500

    Add more tests

--- dwitter/tests/templatetags/test_insert_magic_links.py
+++ dwitter/tests/templatetags/test_insert_magic_links.py
@@ -9,6 +9,24 @@ class DweetTestCase(TestCase):
             insert_magic_links('prefix <h1>content</h1> suffix')
         )
 
+    def test_insert_magic_links_replaces_user_with_valid_characters(self):
+        self.assertEqual(
+            '<a href="/u/a1_.@+-">/u/a1_.@+-</a>',
+            insert_magic_links('/u/a1_.@+-')
+        )
+
+    def test_insert_magic_links_bypasses_user_with_invalid_characters(self):
+        self.assertEqual(
+            '/u/a1$',
+            '/u/a1$'
+        )
+
+    def test_insert_magic_links_replaces_standalone_user(self):
+        self.assertEqual(
+            '<a href="/u/username">/u/username</a>',
+            insert_magic_links('/u/username')
+        )
+
     def test_insert_magic_links_replaces_user_at_start_of_string(self):
         self.assertEqual(
             '<a href="/u/username">/u/username</a> suffix',
@@ -39,6 +57,24 @@ class DweetTestCase(TestCase):
             insert_magic_links('prefix /u/username/suffix')
         )
 
+    def test_insert_magic_links_replaces_dweet_with_valid_characters(self):
+        self.assertEqual(
+            '<a href="/d/1234567890">/d/1234567890</a>',
+            insert_magic_links('/d/1234567890')
+        )
+
+    def test_insert_magic_links_bypasses_dweet_with_invalid_characters(self):
+        self.assertEqual(
+            '/d/1a',
+            '/d/1a'
+        )
+
+    def test_insert_magic_links_replaces_standalone_dweet(self):
+        self.assertEqual(
+            '<a href="/d/1">/d/1</a>',
+            insert_magic_links('/d/1')
+        )
+
     def test_insert_magic_links_replaces_dweet_at_start_of_string(self):
         self.assertEqual(
             '<a href="/d/1">/d/1</a> suffix',
@@ -67,4 +103,12 @@ class DweetTestCase(TestCase):
         self.assertEqual(
             'prefix /d/1/suffix',
             insert_magic_links('prefix /d/1/suffix')
+        )
+
+    def test_insert_magic_links_mixed(self):
+        self.assertEqual(
+            '<a href="/u/john">/u/john</a> remixed '
+            '<a href="/d/123">/d/123</a> by '
+            '<a href="/u/jane">/u/jane</a>',
+            insert_magic_links('/u/john remixed /d/123 by /u/jane')
         )
