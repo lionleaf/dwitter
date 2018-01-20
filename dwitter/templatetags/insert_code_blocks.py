4bee080d150ae8930447de91bf3b6c328a73e2a5
Ates Goral <ates.goral@shopify.com>
2018-01-20 16:02:27 -0500

    Backticks for code blocks

---
+++ dwitter/templatetags/insert_code_blocks.py
@@ -0,0 +1,25 @@
+import re
+from django import template
+from django.utils.html import escape
+from django.utils.safestring import mark_safe
+
+register = template.Library()
+
+def to_code_block(m):
+    code = m.group('code')
+    code = re.sub(r'\\`', '`', code)
+
+    return '<code>%s</code>' % (escape(code))
+
+
+@register.filter
+def insert_code_blocks(text):
+    result = re.sub(
+        r'`'            # start with `
+        r'(?P<code>.*)' # capture code block
+        r'(?<!\\)'      # not preceded by \
+        r'`',           # end with `
+        to_code_block,
+        text
+    )
+    return mark_safe(result)
