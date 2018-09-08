f7fee5d3beef3a785a87589cd8303feb0da04caa
Doggoduck <34210285+kipkat@users.noreply.github.com>
2018-09-08 03:22:16 -0700

    Add custom expander key & improve templates

--- dwitter/static/js/expander.js
+++ dwitter/static/js/expander.js
@@ -1,37 +1,34 @@
-editor = document.getElementById("editor");
-function u_repl(str,old_,new_)
-{
-  return str.split(old_).join(new_);
-}
+if(localStorage.getItem("expandKey")==null) localStorage.setItem("expandKey"," ")
+expKey = localStorage.getItem("expandKey")
+editor = document.getElementById("editor")
+function u_repl(str,old_,new_){return str.split(old_).join(new_)}
 function expand(c)
 {
-    var start = c.selectionStart,
-    	end = c.selectionEnd;
-    c.value = u_repl(c.value,"hsl;","`hsl(${},99%,50%)`;");
-    c.value = u_repl(c.value,"hsla;","`hsla(${},99%,50%,.5)`;");
-    c.value = u_repl(c.value,"x.fs=","x.fillStyle=");
-    c.value = u_repl(c.value,"x.ss=","x.strokeStyle=");
-    c.value = u_repl(c.value,"x.fr","x.fillRect(");
-    c.value = u_repl(c.value,"x.bp;","x.beginPath();");
-    c.value = u_repl(c.value,"x.fl;","x.fill();");
-    c.value = u_repl(c.value,"x.sk;","x.stroke();");
-    c.value = u_repl(c.value,"x.sr","x.strokeRect(");
-    c.value = u_repl(c.value,"clear;","c.width^=0;");
-    c.value = u_repl(c.value,"middle;","x.translate(960,540);");
-    c.value = u_repl(c.value,"1time","t?0:event;");
-    c.value = u_repl(c.value,"x.gcop=","x.globalCompositeOperation=");
-    c.value = u_repl(c.value,"x.dri","x.drawImage(c,0,0)");
-    c.value = u_repl(c.value,"m.ab","Math.abs()");
-    c.value = u_repl(c.value,"m.rn","Math.random()");
-    c.value = u_repl(c.value,"..rb",Math.random()<0.5?'1':'0');
-    c.value = u_repl(c.value,"..for","for(i=0;i++<10;)");
-    c.value = u_repl(c.value,"gr1","α");
-    c.value = u_repl(c.value,"gr2","β");
-    c.value = u_repl(c.value,"gr3","θ");
-    c.value = u_repl(c.value,"gr4","τ");
-    c.value = u_repl(c.value,"gr5","λ");
-    c.value = u_repl(c.value,";\n",";");
-    c.value = u_repl(c.value,"  ",",");
+    var start = c.selectionStart, end = c.selectionEnd;
+    c.value = u_repl(c.value,"hsl;"+expKey,"`hsl(${},99%,50%)`;")
+    c.value = u_repl(c.value,"hsla;"+expKey,"`hsla(${},99%,50%,.5)`;")
+    c.value = u_repl(c.value,"x.fs="+expKey,"x.fillStyle=;")
+    c.value = u_repl(c.value,"x.ss="+expKey,"x.strokeStyle=;")
+    c.value = u_repl(c.value,"x.fr"+expKey,"x.fillRect(x,y,w,h);")
+    c.value = u_repl(c.value,"x.bp;"+expKey,"x.beginPath();")
+    c.value = u_repl(c.value,"x.fl;"+expKey,"x.fill();")
+    c.value = u_repl(c.value,"x.sk;"+expKey,"x.stroke();")
+    c.value = u_repl(c.value,"x.sr"+expKey,"x.strokeRect(x,y,w,h);")
+    c.value = u_repl(c.value,"clear;"+expKey,"c.width^=0;")
+    c.value = u_repl(c.value,"middle;"+expKey,"x.translate(960,540);")
+    c.value = u_repl(c.value,"1time"+expKey,"t?0:event;")
+    c.value = u_repl(c.value,"x.gcop="+expKey,"x.globalCompositeOperation=;")
+    c.value = u_repl(c.value,"x.dri"+expKey,"x.drawImage(c,0,0);")
+    c.value = u_repl(c.value,"m.ab"+expKey,"Math.abs(n);")
+    c.value = u_repl(c.value,"m.rn"+expKey,"Math.random(n);")
+    c.value = u_repl(c.value,"..rb"+expKey,Math.random()<.5?'1':'0')
+    c.value = u_repl(c.value,"..for"+expKey,"for(i=0;i++<10;)")
+    c.value = u_repl(c.value,"gr1"+expKey,"α")
+    c.value = u_repl(c.value,"gr2"+expKey,"β")
+    c.value = u_repl(c.value,"gr3"+expKey,"θ")
+    c.value = u_repl(c.value,"gr4"+expKey,"τ")
+    c.value = u_repl(c.value,"gr5"+expKey,"λ")
+    c.value = u_repl(c.value,";\n",";")
     c.setSelectionRange(start, end);
 }
-setInterval(function() { expand(editor); },50);
+setInterval(function(){expand(editor)},50)
