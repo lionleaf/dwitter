cc62f5a4b366cbcb50b5fded9e9ad0cc3d5f6425
dwitterer <36137355+dwitterer@users.noreply.github.com>
2018-10-26 00:56:51 +0100

    Update hashtag regex
    The new restrictions I added should greatly reduce the number of false-positive hashtag identifications, while obviously still keeping existing hashtags working. Also removed some redundant syntax, making the regex nicer, and making it easier to remove the "hashtags cannot start with a digit" rule if we ever decide to do that.

--- dwitter/templatetags/insert_magic_links.py
+++ dwitter/templatetags/insert_magic_links.py
@@ -21,10 +21,10 @@ def user_dweet_to_link(m):
 def hashtag_to_link(m):
     text = m.group('text')
     hashtag = m.group('hashtag')
-
+    
     url = 'h/' + hashtag
     tag = '#' + hashtag
-
+    
     result = '<a href="/{0}">{1}</a>'.format(url, tag)
     return text.replace(tag, result)
 
@@ -32,19 +32,19 @@ def hashtag_to_link(m):
 @register.filter(is_safe=True)
 def insert_magic_links(text):
     text = re.sub(
-        r'(?:^|(?<=\s))'                                        # start of string or whitespace
-        r'/?'                                                   # optional /
-        r'(?P<text>'                                            # capture original pattern
-        r'[^a-zA-Z\d]?d/(?P<dweet_id>\d+)[^a-zA-Z]?'            # dweet reference
-        r'|'                                                    # or
-        r'[^a-zA-Z\d]?u/(?P<username>[\w.@+-]+)[^a-zA-Z\d]?)'   # user reference
-        r'(?=$|\s|#)',                                       # end of string, whitespace or hashtag
+        r'(?:^|(?<=\s))'                                       # start of string or whitespace
+        r'/?'                                                  # optional /
+        r'(?P<text>'                                           # capture original pattern
+        r'[^a-zA-Z\d]?d/(?P<dweet_id>\d+)[^a-zA-Z]?'           # dweet reference
+        r'|'                                                   # or
+        r'[^a-zA-Z\d]?u/(?P<username>[\w.@+-]+)[^a-zA-Z\d]?)'  # user reference
+        r'(?=$|\s|#)',                                         # end of string, whitespace or hashtag
         user_dweet_to_link,
         text
     )
     text = re.sub(
-        r'(?P<text>'                                            # capture original pattern
-        r'#(?P<hashtag>[_a-zA-Z][_a-zA-Z\d]*)[^_a-zA-Z\d]?)',   # hashtag
+        r'(?P<text>'                                      # capture original pattern
+        r'(?<!\S)#(?!\d)(?P<hashtag>[_a-zA-Z\d]{2,})\b',  # hashtag - check for whitespace precedence and make sure length >2
         hashtag_to_link,
         text
     )
