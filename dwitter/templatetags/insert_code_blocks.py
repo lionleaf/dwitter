c2a9df1dfbf62e0d37941b66e024ebec8adc62c1
dwitterer <36137355+dwitterer@users.noreply.github.com>
2018-10-26 14:59:28 +0100

    Fix other lint-python errors

--- dwitter/templatetags/insert_code_blocks.py
+++ dwitter/templatetags/insert_code_blocks.py
@@ -4,15 +4,14 @@ from django.utils.safestring import mark_safe
 
 register = template.Library()
 
-
 def to_code_block(m):
     code = m.group('code')
     code = re.sub(r'\\`', '`', code)
-    
-    # anchor annihilator - ruthlessly removes all anchors (<a>) from the code, using the magic of regex... it shows NO remorse!
+
+    # remove anchors from code blocks
     a = re.findall(r'(<a[^<]*>(?P<link>[^<]*)</a>)', code)
     for i in range(len(a)):
-	    code = code.replace(a[i][0], a[i][1])
+        code = code.replace(a[i][0], a[i][1])
 
     return '<code>%s</code>' % code
 
