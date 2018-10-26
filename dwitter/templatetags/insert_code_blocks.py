a80595af134d86d22258ea3c87b62bcb635cc1f2
dwitterer <36137355+dwitterer@users.noreply.github.com>
2018-10-26 01:49:55 +0100

    Summon the anchor annihilator
    Makes sure there's no clickable pollutants in our code blocks

--- dwitter/templatetags/insert_code_blocks.py
+++ dwitter/templatetags/insert_code_blocks.py
@@ -8,6 +8,11 @@ register = template.Library()
 def to_code_block(m):
     code = m.group('code')
     code = re.sub(r'\\`', '`', code)
+    
+    # anchor annihilator - ruthlessly removes all anchors (<a>) from the code, using the magic of regex... it shows NO remorse!
+    a = re.findall(r'(<a[^<]*>(?P<link>[^<]*)</a>)', code)
+    for i in range(len(a)):
+	    code = code.replace(a[i][0], a[i][1])
 
     return '<code>%s</code>' % code
 
