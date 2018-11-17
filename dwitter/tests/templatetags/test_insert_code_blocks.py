e6b5431fc83a22184990f9d72f718d6ba9a24586
dwitterer <36137355+dwitterer@users.noreply.github.com>
2018-11-17 20:53:41 +0000

    Update test_insert_code_blocks.py

--- dwitter/tests/templatetags/test_insert_code_blocks.py
+++ dwitter/tests/templatetags/test_insert_code_blocks.py
@@ -3,17 +3,20 @@ from dwitter.templatetags.insert_code_blocks import insert_code_blocks
 
 
 class DweetTestCase(TestCase):
+    
     def test_insert_code_blocks_wraps_stuff_inside_backticks(self):
         self.assertEqual(
             '<code>a</code>',
             insert_code_blocks('`a`')
         )
 
+
     def test_insert_code_blocks_ignores_backticks(self):
         self.assertEqual(
             '<code>a=`b`</code>',
             insert_code_blocks('`a=\`b\``')
         )
+
 
     def test_insert_code_blocks_is_not_greedy_with_multiple_blocks(self):
         self.assertEqual(
