3ff794d82be8cd34f6d6117ddee817e54adea43c
dwitterer <36137355+dwitterer@users.noreply.github.com>
2018-11-20 18:06:55 +0000

    Add new test cases for the anchor annihilator

--- dwitter/tests/templatetags/test_insert_code_blocks.py
+++ dwitter/tests/templatetags/test_insert_code_blocks.py
@@ -4,22 +4,35 @@ from dwitter.templatetags.insert_code_blocks import insert_code_blocks
 
 class DweetTestCase(TestCase):
     
-    def test_insert_code_blocks_wraps_stuff_inside_backticks(self):
+    def test_insert_code_wraps_stuff_inside_backticks(self):
         self.assertEqual(
             '<code>a</code>',
             insert_code_blocks('`a`')
         )
 
 
-    def test_insert_code_blocks_ignores_backticks(self):
+    def test_insert_code_ignores_backticks(self):
         self.assertEqual(
             '<code>a=`b`</code>',
             insert_code_blocks('`a=\`b\``')
         )
 
 
-    def test_insert_code_blocks_is_not_greedy_with_multiple_blocks(self):
+    def test_insert_code_is_not_greedy_with_multiple_blocks(self):
         self.assertEqual(
             'a <code>1</code> b <code>2</code> c',
             insert_code_blocks('a `1` b `2` c')
         )
+        
+    def test_insert_code_removes_anchors(self):
+        self.assertEqual(
+            'My sneaky link here, and also here.',
+            insert_code_blocks('My sneaky <a href="link">link</a> here, and also <a href="link2">here</a>.')
+        )
+        
+    def test_insert_code_with_no_code(self):
+        try:
+            insert_code_blocks('')
+            insert_code_blocks('No code here, move along!')
+        except:
+            self.fail()
