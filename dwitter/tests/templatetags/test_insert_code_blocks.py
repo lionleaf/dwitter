400f1b4135232894aaf0dd1a13871128b31777d5
dwitterer <36137355+dwitterer@users.noreply.github.com>
2018-11-20 19:52:57 +0000

    Fixed lint-python errors in this file
    W293 x2, E501

--- dwitter/tests/templatetags/test_insert_code_blocks.py
+++ dwitter/tests/templatetags/test_insert_code_blocks.py
@@ -21,17 +21,17 @@ class DweetTestCase(TestCase):
             'a <code>1</code> b <code>2</code> c',
             insert_code_blocks('a `1` b `2` c')
         )
-    
+
     def test_insert_code_removes_anchors(self):
         self.assertEqual(
-            'My sneaky link here, and also here.',
-            insert_code_blocks('My sneaky <a href="1">link</a> here, and also <a href="2">here</a>.')
+            'My sneaky link here, and also here',
+            insert_code_blocks('My sneaky <a href="1">link</a> here, and also <a href="2">here</a>')
         )
-    
+
     def test_insert_code_with_no_code(self):
         try:
             insert_code_blocks('')
             insert_code_blocks('No code here, move along!')
         except:
-            self.fail("insert_code_blocks failed to parse a blank comment, " +
+            self.fail("insert_code_blocks() threw an exception with a blank block, " +
                       "or a code block with non-code text.")
