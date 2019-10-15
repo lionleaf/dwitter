f13370c3a4ac48cb70b2d627aa1a11af6a30bbd9
Ben <12856904+ben221199@users.noreply.github.com>
2019-10-15 20:21:44 +0200

    Update urls.py

--- dwitter/api/urls.py
+++ dwitter/api/urls.py
@@ -3,9 +3,14 @@ from rest_framework.routers import DefaultRouter
 from . import views
 from ..feed.views import NewUserFeed, HotUserFeed, TopUserFeed, NewLikedFeed
 
-router = DefaultRouter()
-router.register(r'comments', views.CommentViewSet)
-router.register(r'dweets', views.DweetViewSet)
-router.register(r'users', views.UserViewSet)
+api_old = DefaultRouter()
+api_old.register(r'comments', views.CommentViewSet)
+api_old.register(r'dweets', views.DweetViewSet)
+api_old.register(r'users', views.UserViewSet)
 
-urlpatterns = router.urls
+api_v1 = DefaultRouter()
+
+urlpatterns = [
+  path('', include(api_old.urls)),
+  url(r'^v1/', include(api_v1.urls)),
+]
