61d26bbbc83e05127eff6d6f3b23b6de11b4313a
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2019-12-27 13:05:09 +0100

    New dweet button now gets disabled at >140 chars again

--- dwitter/static/js/feed.js
+++ dwitter/static/js/feed.js
@@ -1,7 +1,7 @@
 function onDweetChanged() {
   // eslint-disable-next-line newline-per-chained-call
   var charCount = $(this).parent().parent().parent().find('.character-count')[0];
-  var submitButton = $(this).parent().parent().find('.remix-button')[0];
+  var submitButton = $(this).parent().parent().find('.remix-button,.dweet-button')[0];
 
   let CountCharacters=string=>
   {
