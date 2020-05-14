29d36bfe96bd08c2ae51b128916ecc80fd3289aa
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2020-05-14 23:08:58 +0200

    Re-add session authentication to DRF

--- dwitter/settings/base.py
+++ dwitter/settings/base.py
@@ -71,7 +71,7 @@ REST_FRAMEWORK = {
     'PAGE_SIZE': 10,                   # Default to 10
     'MAX_PAGE_SIZE': 100,             # Maximum limit allowed when using `?page_size=xxx`.
     'DEFAULT_RENDERER_CLASSES': ('rest_framework.renderers.JSONRenderer',),
-    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication', )
+    'DEFAULT_AUTHENTICATION_CLASSES': ('rest_framework.authentication.TokenAuthentication','rest_framework.authentication.SessionAuthentication', )
 
 }
 
