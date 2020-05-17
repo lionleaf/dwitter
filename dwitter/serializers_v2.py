6aa98b8d7876f0995647baa9b5eb983c2a2408a7
Andreas Løve Selvik <andreas.l.selvik@gmail.com>
2020-05-16 20:17:56 -0700

    Change the remix-of field to embed the dweet instead of just pk

--- dwitter/serializers_v2.py
+++ dwitter/serializers_v2.py
@@ -29,12 +29,41 @@ class CommentSerializer(serializers.ModelSerializer):
         )
 
 
-class DweetSerializer(serializers.ModelSerializer):
+class RemixOfSerializer(serializers.ModelSerializer):
+    """ Dweet serializer for embedding dweets in the remix_of field """
+
     id = serializers.ReadOnlyField(source='pk')
+
+    # Don't continue the chain of embedded remixes
+    # and only include the ID of the next dweet
     remix_of = serializers.PrimaryKeyRelatedField(
         source='reply_to',
         queryset=Dweet.with_deleted.all()
     )
+
+    awesome_count = serializers.SerializerMethodField()
+    author = UserSerializer()
+
+    class Meta:
+        model = Dweet
+        fields = (
+            'author',
+            'awesome_count',
+            'code',
+            'id',
+            'posted',
+            'remix_of',
+        )
+
+    def get_awesome_count(self, obj):
+        return obj.likes.all().count()
+
+
+class DweetSerializer(serializers.ModelSerializer):
+    id = serializers.ReadOnlyField(source='pk')
+
+    remix_of = RemixOfSerializer(source='reply_to')
+
     awesome_count = serializers.SerializerMethodField()
     has_user_awesomed = serializers.SerializerMethodField()
     author = UserSerializer()
