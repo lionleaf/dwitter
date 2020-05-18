bc434b4816af2775d7758bd1b4e57102d4c21a16
Sigve Sebastian Farstad <sigvefarstad@gmail.com>
2020-05-18 19:49:14 +0200

    Add set email and password to apiv2

--- dwitter/serializers_v2.py
+++ dwitter/serializers_v2.py
@@ -2,6 +2,9 @@ from rest_framework import serializers
 from dwitter.models import Comment, Dweet
 from dwitter.templatetags.to_gravatar_url import to_gravatar_url
 from django.contrib.auth.models import User
+from rest_framework.exceptions import ValidationError
+from django.contrib.auth.password_validation import validate_password
+from rest_framework.fields import SkipField
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -9,10 +12,46 @@ class UserSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = User
-        fields = ('id', 'username', 'avatar')
+        read_only_fields = ('id', 'username', 'avatar')
+        fields = read_only_fields
+        
 
     def get_avatar(self, obj):
         return to_gravatar_url(obj.email)
+
+    def to_representation(self, instance):
+        value = super().to_representation(instance)
+        user = self.context['request'].user
+        if user.is_authenticated and user.id == instance.id:
+            value['email'] = user.email
+        return value
+
+
+class SetEmailSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ('email',)
+
+
+class SetPasswordSerializer(serializers.Serializer):
+    old_password = serializers.CharField()
+    new_password = serializers.CharField()
+
+    def validate_old_password(self, old_password):
+        user = self.context['request'].user
+        if not user.check_password(old_password):
+            raise ValidationError('Wrong password.')
+        return old_password
+
+    def validate_new_password(self, new_password):
+        validate_password(new_password)
+        return new_password
+
+    def save(self):
+        user = self.context['request'].user
+        user.set_password(self.validated_data['new_password'])
+        user.save()
+        return user
 
 
 class CommentSerializer(serializers.ModelSerializer):
