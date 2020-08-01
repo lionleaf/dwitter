c120aa373a1b2f1e22196653344d2cf0654a629b
Ben <ben@yocto.nu>
2020-08-01 11:19:38 +0200

    Add relationship endpoints

--- dwitter/settings/base.py
+++ dwitter/settings/base.py
@@ -45,6 +45,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'django.contrib.sites',
+    'django_extensions',
     'django_filters',
     'rest_framework',
     'rest_framework.authtoken',
--- dwitter/urls_dwitter_v1.py
+++ dwitter/urls_dwitter_v1.py
@@ -1,5 +1,5 @@
 from django.conf.urls import include, url
-from dwitter.views import CommentViewSet, DweetViewSet, HashtagViewSet, UserViewSet
+from dwitter.views import CommentRelationshipView, CommentViewSet, DweetRelationshipView, DweetViewSet, HashtagRelationshipView, HashtagViewSet, UserRelationshipView, UserViewSet
 from rest_framework import routers
 
 router = routers.DefaultRouter(trailing_slash=False)
@@ -11,4 +11,18 @@ router.register(r'users', UserViewSet)
 
 urlpatterns = [
     url(r'^', include(router.urls)),
+    
+    
+    url(r'^comments/(?P<pk>[^/.]+)/relationships/(?P<related_field>\w+)$',
+        CommentRelationshipView.as_view(),
+        name='blog-relationships'),
+    url(r'^dweets/(?P<pk>[^/.]+)/relationships/(?P<related_field>\w+)$',
+        DweetRelationshipView.as_view(),
+        name='blog-relationships'),
+    url(r'^hashtags/(?P<pk>[^/.]+)/relationships/(?P<related_field>\w+)$',
+        HashtagRelationshipView.as_view(),
+        name='blog-relationships'),
+    url(r'^users/(?P<pk>[^/.]+)/relationships/(?P<related_field>\w+)$',
+        UserRelationshipView.as_view(),
+        name='blog-relationships'),
 ]
