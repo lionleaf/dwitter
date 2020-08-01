7249ba24d85ffff76617fdbd59ad759d3659cf50
Ben <ben@yocto.nu>
2020-08-01 14:41:00 +0200

    Remove unused files

--- dwitter/user/forms.py
+++
@@ -1,8 +0,0 @@
-from django.contrib.auth import get_user_model
-from django.forms import ModelForm
-
-
-class UserSettingsForm(ModelForm):
-    class Meta:
-        model = get_user_model()
-        fields = ('email',)
